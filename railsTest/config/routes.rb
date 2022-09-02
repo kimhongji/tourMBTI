82c7f925e9cff90fb1788c5ca3c12849fe2b253c
hayz-de <hayz.de@kakaoenterprise.com>
2022-09-02 10:32:26 +0900

    add map

--- railsTest/config/routes.rb
+++ railsTest/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get '' => 'home#index'
+  get '/map' => 'map#map'
   resources :tour_keywords
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- railsTest/db/schema.rb
+++ railsTest/db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20220807092921) do
+ActiveRecord::Schema.define(version: 2022_08_07_092921) do
 
   create_table "tour_keywords", force: :cascade do |t|
     t.string "name"
-    t.text   "keywords"
+    t.text "keywords"
   end
 
 end
